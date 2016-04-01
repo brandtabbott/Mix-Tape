281b0361da06fdde54c41e05c9c3e0a887e1da40
Brandt Abbott <brandt.abbott@readytalk.com>
2016-04-01 09:54:04 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Mix-Tape
+
+![Mix-Tape](https://raw.github.com/brandtabbott/mix-tape/master/public/mixtape.gif)
+================
+Make a mix tape for your loved one!!  I love mix tapes.
+
+Installation
+------------
+
+Execute:
+
+    $ npm install
+    $ npm start
+
+Goto URL:
+
+    http://localhost:3000
+
+
+License
+-------
+Copyright (c) 2013 Brandt Abbott. Distributed under the MIT License.
---
+++ app.js
@@ -0,0 +1,39 @@
+var color = require('tinycolor'),
+    cookieParser = require('cookie-parser'), 
+    ejs = require('ejs'),
+    express = require('express'),    
+    fs = require('fs')
+    http = require('http'),
+    path = require('path');
+    
+//Don't stop this server if an exception goes uncaught
+process.on('uncaughtException', function (err) {
+  console.error((err.stack+'').red.bold);
+  console.error('Node trying not to exit...'.red.bold);
+});     
+
+var app = express();
+app.set('port', 3000);
+app.set('views', __dirname+'/views');
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, '/public')));
+
+var contentDirectory = '/public/mixtape/';
+var files = [];
+fs.readdir(__dirname+contentDirectory, function(err,list){
+  if(err) throw err;
+  for(var i=0; i<list.length; i++){
+    console.log(list[i])
+    files.push('"'+contentDirectory.replace('/public', '')+list[i]+'"');  
+  }
+});
+
+app.get('/', function(req, res){
+  res.locals.tracks=files;
+  res.render('index');
+});
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+  console.log('Listening on port',app.get('port'));
+});
